fe01d5db2e688f3918b237f0e1a2331e590fc2ba
Richard Tsai <richtxo@gmail.com>
2019-11-03 10:10:33 -0500

    adding mysql functions

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const mysql = require('mysql');
 const path = require('path');
 const app = express();
 
-//const sqlInsert = require('./sqlInsert');
-//const sqlDelete = require('./sqlDelete');
-//const sqlSelect = require('./sqlSelect');
-
 
 // Express Middleware for serving static files
 app.use(express.static(path.join(__dirname, '/Public')));
@@ -17,6 +13,55 @@ app.use(express.static(path.join(__dirname, '/Public')));
 app.get('/', function(req, res) {
     res.redirect('index.html');
 });
+
+
+var pool = mysql.createPool({
+    connectionLimit : 100, //important
+    host     : 'localhost',
+    user     : 'root',
+    password : 'dataispet',
+    database : 'petData'
+});
+
+
+function handle_database(req,res) {
+    // connection will be acquired automatically
+    pool.query("select * from pet",function(err,rows){
+        if(err) {
+            return res.json({'error': true, 'message': 'Error occurred'+err});
+        }
+        //connection will be released as well.
+        res.json(rows);
+    });
+}
+
+
+function addRow(data) {
+    let insertQuery = 'INSERT INTO pet VALUES (?,?,?,?,?,?)';
+    let query = mysql.format(insertQuery,[data.name,data.petType, data.sex, data.locFound, data.locNow, data.date]);
+    pool.query(query,(err, response) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        // rows added
+        console.log(response.insertId);
+    });
+}
+
+
+function deleteRow(userName) {
+    let deleteQuery = "DELETE from pet where name=? and petType=? and sex=? and locationFound=? and locationNow=? date=?";
+    let query = mysql.format(deleteQuery, [data.name,data.petType, data.sex, data.locFound, data.locNow, data.date]);
+    pool.query(query,(err, response) => {
+        if(err) {
+            console.error(err);
+            return;
+        }
+        // rows deleted
+        console.log(response.affectedRows);
+    });
+}
 
 
 // Start the server
